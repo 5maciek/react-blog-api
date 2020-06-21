2f0dabad10787f629a83f4eec6b73f300c88ad4a
5maciek <smaciek1@op.pl>
2020-06-21 20:11:56 +0200

    add comments component

---
+++ src/Components/Comments.js
@@ -0,0 +1,43 @@
+import React, {  useEffect, useState } from 'react';
+import AddComment from './AddComment';
+
+export default function Comments(props) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const { postId } = props;
+    if (postId !== undefined && postId !== null) {
+      fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+        .then((response) => response.json())
+        .then((response) => {
+          setComments(response);
+        })
+        .catch((error) => console.log(error));
+    }
+
+    return () => {
+      setComments(null);
+    };
+  }, [props.showComments]);
+
+  return (
+    <section className="comments">
+      {props.showComments ? (
+        <ul className="comments__list">
+          <>
+            {comments
+              ? comments.map((comment) => (
+                  <li key={comment.id}>
+                    <h4>{comment.name}</h4>
+                    <h4>{comment.email}</h4>
+                    <p> {comment.body}</p>
+                  </li>
+                ))
+              : null}
+            <AddComment postId={props.postId} />
+          </>
+        </ul>
+      ) : null}
+    </section>
+  );
+}
